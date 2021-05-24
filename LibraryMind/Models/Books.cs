1aeba488c6a2e406e4f3430cba26de2aaa9e853b
Lizug12 <liza-lis-2002@mail.ru>
2021-05-24 11:47:26 +0500

    Commit1

---
+++ LibraryMind/Models/Books.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryMind.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Введите название книги")]
+        [Display(Name = "Название книги")]
+        public string BookName { get; set; }
+
+        [Required(ErrorMessage = "Введите автора")]
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Введите жанр")]
+        [Display(Name = "Жанр")]
+        public string Genre { get; set; }
+
+        //public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
